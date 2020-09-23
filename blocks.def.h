65de75c698923fd064402b6e9efe0a192bfdecf7
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-09-24 05:44:54 +0800

    Change nettraf with my own net wlan trafic mon

--- blocks.def.h
+++ blocks.def.h
@@ -5,7 +5,7 @@ static const Block blocks[] = {
 	{"",        "$HOME/bin/status/tor",                     1,  	            4},
 	{"",        "$HOME/bin/status/vpn",                     1,  	            3},
 	{"",        "$HOME/bin/status/key-mon",                 0,  	            2},
-	{"",        "$HOME/bin/status/nettraf",                 1,		            0},
+	{"",        "$HOME/bin/status/network-wlan-tfc",        1,		            0},
 	{"",        "$HOME/bin/status/cpu-temp",                1,		            0},
 	{"",        "$HOME/bin/status/memory",	                1,		            0},
 	{"",        "$HOME/bin/status/filesystem",	            3600,	            0},
