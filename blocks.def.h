6c74bd4f0adff6d60706d68fd8513a1ece8d1f5a
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-12-01 01:29:00 +0800

    Add module misc

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		                            /*Update Interval*/	/*Update Signal*/
+	{"",        "$HOME/bin/status/misc",                    1,  	            7},
 	{"",        "$HOME/bin/status/record",                  1,  	            5},
 	{"",        "$HOME/bin/status/tor",                     1,  	            4},
 	{"",        "$HOME/bin/status/vpn",                     1,  	            3},
